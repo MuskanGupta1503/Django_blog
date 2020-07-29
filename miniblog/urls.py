300fc03350648c30ee7fc23d663038fc6c4c5fa5
Muskan Gupta <gupta.muskan02000@gmail.com>
2020-07-30 01:19:54 +0530

    blog.

--- miniblog/urls.py
+++ miniblog/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from miniblog.blog import views
+from ..blog import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
