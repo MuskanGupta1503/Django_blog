6ef72de3796d946cd85a4322c7d16b3005b917a5
Muskan Gupta <gupta.muskan02000@gmail.com>
2020-07-30 00:26:10 +0530

    in

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-import django_heroku
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -29,7 +29,7 @@ DEBUG=True
 
 # ALLOWED_HOSTS = ['https://django-blog12.herokuapp.com/','127.0.0.1']
 # ALLOWED_HOSTS=[]
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://damp-reaches-70623.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
